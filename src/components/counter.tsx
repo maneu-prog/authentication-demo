bb6da8ea165d40a2853756ba6441b5c06cb84936
maneu-prog <152749019+maneu-prog@users.noreply.github.com>
2025-07-11 14:12:59 -0300

    first commit

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { useAuth, useUser } from '@clerk/nextjs'
+//import { useAuth, useUser } from '@clerk/nextjs'
+import { useUser } from '@clerk/nextjs'
 import { useState } from 'react'
 
 export const Counter = () => {
   const [count, setCount] = useState(0)
   //const { isLoaded, userId, sessionId, getToken } = useAuth()
-  const { isLoaded, isSignedIn, user } = useUser()
+  //const { isLoaded, isSignedIn, user } = useUser()
+  const { isLoaded, isSignedIn } = useUser()
   if (!isLoaded || !isSignedIn) {
     //if (!isLoaded || !userId) {
     return null
